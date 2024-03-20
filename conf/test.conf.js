d69f487553a78f55f842f8ac6b82d2c65f097fc1
manoj-browserstack <manoj.m@browserstack.com>
2024-03-20 06:02:27 +0530

    wdio to

---
+++ conf/test.conf.js
@@ -0,0 +1,41 @@
+const { config: baseConfig } = require('./base.conf.js');
+
+const parallelConfig = {
+  // maxInstances: 10,
+  commonCapabilities: {
+    'bstack:options': {
+      buildName: 'Sample webdriverIO testing',
+      source: 'webdriverio:sample-master:v1.2'
+    }
+  },
+  services: [
+    [
+      'browserstack',
+      { buildIdentifier: '#${BUILD_NUMBER}',
+      testObservability: true,
+      testObservabilityOptions: {
+        'projectName': 'Sample WDIO project TO',
+        'buildName': 'Sample WDIO build',
+        // 'buildTag': 'Any build tag goes here. For e.g. commit message'
+    },},
+    ],
+  ],
+  capabilities: [
+    {
+      browserName: 'chrome',
+      browserVersion: 'latest',
+      'bstack:options': {
+        os: 'Windows',
+        osVersion: '10',
+      },
+    },
+  ],
+};
+
+exports.config = { ...baseConfig, ...parallelConfig };
+
+// Code to support common capabilities
+exports.config.capabilities.forEach(function (caps) {
+  for (var i in exports.config.commonCapabilities)
+    caps[i] = { ...caps[i], ...exports.config.commonCapabilities[i]};
+});
